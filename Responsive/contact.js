b0f98a1c32db8aa2838a319d6431d4e0c00f4af8
maciejwojtas <38115309+maciejwojtas@users.noreply.github.com>
2018-04-07 19:24:51 +0200

    0.3
    contact page

---
+++ Responsive/contact.js
@@ -0,0 +1,39 @@
+$(".panel").toggle()
+$(".row").toggle()
+
+
+
+
+
+$("#menu").click( function(){
+    $(".panel").toggle()
+})
+
+
+$("hgroup a").click( function(){
+    $(".row").toggle()
+        $("hgroup").toggle()
+
+})
+   
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+   
+     
+                          
+        
+        
